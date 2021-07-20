1c6f81132b64e6a31bef301cda5bc5f76fe2694b
Vishwnath@1998 <telangvishwnath@1998gmail.com>
2021-07-20 15:49:27 +0530

    final

--- api.js
+++ api.js
@@ -1,54 +1,58 @@
-
+var arr=[];
 let reRenderStar=()=>{
     let point = ` https://swapi.dev/api/people/`;
        let cardUI = "";
        fetch(point)
     .then((response) => response.json())
     .then((data) => {
+        arr.push(data);
         console.log(data);
      data.results.forEach((item, index) => {
       cardUI += `<div class="card" key=${index}>
-      <p>name:${item.name}</p>
-      <p>height:${item.height}</p>
-      <p>mass:${item.mass}</p>
-      <p>hair_color:${item.hair_color}</p>
-      <p>skin_colo:${item.skin_colo}</p>
-      <p>eye_color:${item.eye_color}</p>
-      <p>birth_year:${item.birth_year}</p>
-      <p>gender:${item.gender}</p>
-      <button onclick="deleted(${index,data.results})"> Delete</button>
+      <p><b>name</b>:${item.name}</p>
+      <p><b>height</b>:${item.height}</p>
+      <p><b>mass</b>:${item.mass}</p>
+      <p><b>hair_color</b>:${item.hair_color}</p>
+      <p><b>skin_colo</b>:${item.skin_colo}</p>
+      <p><b>eye_color</b>:${item.eye_color}</p>
+      <p><b>birth_year</b>:${item.birth_year}</p>
+      <p><b>gender</b>:${item.gender}</p>
+      <button onclick="deleted(${index})"class="btn btn-warning"> Delete</button>
      
   </div>`;
         });
         setTimeout(() => {
             document.querySelector(".stardata").innerHTML = cardUI;
 
-        }, 2000);
+        }, 3000);
     
     })
 };
- reRenderStar();    
-  function reRender(arr){
+ reRenderStar(); 
+ 
+ console.log(arr);
+  function reRender(res){
     let cardUI = "";
-    arr.forEach((item,index) => {
+    res.forEach((item,index) => {
         cardUI += `<div class="card" key=${index}>
-        <p>name:${item.name}</p>
-        <p>height:${item.height}</p>
-        <p>mass:${item.mass}</p>
-        <p>hair_color:${item.hair_color}</p>
-        <p>skin_colo:${item.skin_colo}</p>
-        <p>eye_color:${item.eye_color}</p>
-        <p>birth_year:${item.birth_year}</p>
-        <p>gender:${item.gender}</p>
-        <button onclick="deleted(${index,arr})"> Delete</button>
-       
+        <p><b>name</b>:${item.name}</p>
+      <p><b>height</b>:${item.height}</p>
+      <p><b>mass</b>:${item.mass}</p>
+      <p><b>hair_color</b>:${item.hair_color}</p>
+      <p><b>skin_colo</b>:${item.skin_colo}</p>
+      <p><b>eye_color</b>:${item.eye_color}</p>
+      <p><b>birth_year</b>:${item.birth_year}</p>
+      <p><b>gender</b>:${item.gender}</p>
+      <button onclick="deleted(${index})"class="btn btn-warning"> Delete</button>
+     
     </div>`;
           });
           document.querySelector(".stardata").innerHTML = cardUI;
   }
  
-function deleted(index,arr){
-   arr.splice(index,1)
-    reRender(arr);
+function deleted(index){
+   let res = arr[0].results
+   res.splice(index,1)
+    reRender(res);
 }
    
